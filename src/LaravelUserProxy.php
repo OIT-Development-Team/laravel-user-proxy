8d27a7a551402bcdab81fe6486fb1c6a41e637e5
Brian Jones <brian.jones@ua.edu>
2024-10-15 16:22:15 -0500

    Added initial package files

---
+++ src/LaravelUserProxy.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace OITDevelopmentTeam\LaravelUserProxy;
+
+class LaravelUserProxy
+{
+    public function __construct()
+    {
+        
+    }
+
+}
---
+++ src/LaravelUserProxyFacade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace OITDevelopmentTeam\LaravelUserProxy;
+
+use Illuminate\Support\Facades\Facade;
+
+
+
+
+class LaravelUserProxyFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'laravel-user-proxy';
+    }
+}
